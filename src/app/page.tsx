fc9e03e99add9876a0f5555acfdaa559e5f3cd39
Aditya <adi2652002@gmail.com>
2023-06-09 23:14:03 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,15 +92,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className=-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          hrxt.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
